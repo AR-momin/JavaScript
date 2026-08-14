449fa1a943e9887bb337c86e3feaeab906ec8926
Akhtar Raza <akhtarraza1912@gmail.com>
2026-08-14 21:40:31 +0530

    some exercises on JS variables and Object

---
+++ script.js
@@ -0,0 +1,77 @@
+// let name = "angad Kayak";
+// let age = 21;
+// let course = "BCA";
+// let college = "Birla college Kalyan";
+// let isStudent = true;
+
+// console.log(name , age , course , college , isStudent)
+
+
+// let name = "Akhtar Raza";
+// const age = 19;
+// let city = "Bhiwandi";
+// let course = "BCA";
+// let hobby = "coding";
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+// console.log(course)
+// console.log(hobby)
+
+
+// let a = 19;
+// let b = "25";
+// let c = false;
+// let d;
+// let e = null;
+// let f = 1009.05;
+
+// console.log(typeof a, typeof b,typeof c, typeof d,typeof e,typeof f)
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+// console.log(typeof e)
+// console.log(typeof f)
+
+
+// let student = {
+//   "name": "Akhtar",
+//   "age": 19,
+//   "course": "BCA",
+//   "semester": 3,
+//   "isStudent": true
+// };
+
+// console.log(student.name)
+// console.log(student.age)
+// console.log(student.course)
+// console.log(student.semester)
+// console.log(student.isStudent)
+
+
+
+let student = {
+  "name":"Akhtar Raza",
+  "age":19,
+  "gender":"Male",
+  "course":"SY BCA",
+  "college":"YCMOU",
+  "semester":"3rd",
+  "city":"Bhiwandi",
+  "email":"abc@gmail.com",
+  "phone":"0000000000",
+  "isStudent":true,
+};
+
+console.log(student.name)
+console.log(student.age)
+console.log(student.gender)
+console.log(student.course)
+console.log(student.college)
+console.log(student.semester)
+console.log(student.city)
+console.log(student.email)
+console.log(student.phone)
+console.log(student.isStudent)
