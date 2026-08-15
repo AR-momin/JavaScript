e0f5bcd39b9f8593f32212d0f40307e036ada15e
Akhtar Raza <akhtarraza1912@gmail.com>
2026-08-15 07:52:04 +0530

    practiced exercises on JS conditional statements

---
+++ 56.js
@@ -0,0 +1,79 @@
+// exercise 1
+// let age = 18;
+
+// if (age >= 18) {
+//   console.log("you can vote");
+// }
+// else {
+//   console.log("you cannot vote");
+// }
+
+
+
+// exercise 2
+// const b = 0;
+
+// if(b>0){
+//   console.log("your number is positive");
+// }
+// else if(b<0){
+//   console.log("your number is negative");
+// }
+// else{
+//   console.log("your number is zero");
+// }
+
+
+
+// exercise 3
+// let mark=75;
+
+// if(mark>90){
+//   console.log("Grade A");
+// }
+// else if(mark>=75 && mark<=89){
+//   console.log("Grade B");
+// }
+// else if(mark>=60 && mark<=74){
+//   console.log("Grade C");
+// }
+// else if(mark>=40 && mark<=59){
+//   console.log("Grade D");
+// }
+// else{
+//   console.log("FAIL");
+// }
+
+
+
+// exercise 4
+// let username = "Akhtar raza";
+// let password = 12345678;
+
+// if(username == "Akhtar Raza" && password == 12345678){
+//   console.log("you can move onto the next step");
+// }
+// else{
+//   console.log("invalid password or username");
+// }
+
+
+
+// exercise 5
+// let amount = 999;
+
+// if (amount >= 5000) {
+//   console.log("congratulations you got extra 20% discount");
+// }
+// else if (amount >= 3000 && amount <= 4999) {
+//   console.log("congratulations you got extra 10% discount");
+// }
+// else if (amount >= 1000 && amount <= 2999) {
+//   console.log("congratulations you got extra 5% discount");
+// }
+// else {
+//   console.log("unfortunately you got no discount");
+// }
+
+
+
