748ccf5d28c050a396fd0367ab535f3ea1788760
Akhtar Raza <akhtarraza1912@gmail.com>
2026-08-20 06:59:15 +0530

    Some exercises on Strings

---
+++ 60.js
@@ -0,0 +1,52 @@
+// let s = "Muhammed Akhtar Raza Imran Momin";
+
+// console.log(s.length);
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase());
+
+
+
+// let first = prompt("Enter your First Name");
+// let last = prompt("Enter your Last Name");
+
+// console.log(first.concat(" ",last));
+// console.log(`Hello, ${first} ${last}! welcome to JavaScript`);
+
+
+
+// let a = "JavaScript is very easy to learn";
+
+// console.log(a.replace("easy", "powerful").toUpperCase());
+// console.log(a.slice(0,10));
+
+
+
+
+
+// let first = prompt("Enter your first name:");
+// let last = prompt("Enter your last name:");
+
+// console.log(`${first} ${last}`);
+// console.log(`${first} ${last}`.toUpperCase());
+// console.log(`${first} ${last}`.toLowerCase());
+// console.log(`${first} ${last}`.slice(0,5));
+// console.log(`${first} ${last}`.length);
+
+
+
+
+let full = prompt("Enter your full name");
+let course = prompt("Enter your course name");
+let city = prompt("Enter your city name");
+let clg = prompt("Enter your college name");
+
+console.log(full);
+console.log(course);
+console.log(city);
+console.log(clg);
+console.log(full.toUpperCase());
+console.log(course.toLowerCase());
+console.log(full.length);
+console.log(full.slice(0,3));
+console.log(clg.replace("college", "university"));
+console.log(full.slice(full.length-3));
