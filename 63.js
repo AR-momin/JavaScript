df7dde65f3775ffbc6dbbdc60da76399dd294af2
Akhtar Raza <akhtarraza1912@gmail.com>
2026-08-23 07:27:26 +0530

    more exercises on arrays

--- 63.js
+++ 63.js
@@ -199,30 +199,132 @@
 
 
 // exercise 9
-let i;
+// let i;
 
-let even=0;
-let odd=0;
-let num = Number(prompt("Enter your Number"));
-for (i = 1; i <= num; i++) {
-    console.log(i)
-  if(i % 2 == 0){
-  console.log("even");
-  even=even+1;
+// let even=0;
+// let odd=0;
+// let num = Number(prompt("Enter your Number"));
+// for (i = 1; i <= num; i++) {
+//     console.log(i)
+//   if(i % 2 == 0){
+//   console.log("even");
+//   even=even+1;
+// }
+// else{
+//   console.log("odd")
+//   odd=odd+1;
+// }
+// }
+
+// let sum=0;
+// for (let i = 1; i <= num; i++) {
+//   sum = sum + i;
+// }
+//   console.log("sum of the above number is:",sum);
+//   console.log("even number:",even);
+//   console.log("odd number:",odd);
+
+
+
+
+// // exercise 9.2
+// let num = Number(prompt("Enter your number"));
+// let divby3 = 0;
+// let sumeven = 0;
+// for (let i = 1; i <= num; i++) {
+//   console.log(i);
+
+//   if (i % 3 == 0) {
+//     console.log("divisible by 3");
+//     divby3 = divby3+1;
+//   }
+//   else{
+//     console.log("not divisible by 3");
+//   }
+
+//   if(i % 2 == 0){
+//     console.log("Even number");
+//     sumeven = sumeven +  i;
+//   }
+// }
+
+// console.log("divisible by 3 count is",divby3);
+// console.log("sum of the even numbers are :",sumeven);
+
+
+
+
+
+// // exercise 9.3
+// let num = Number(prompt("Enter your number"));
+// let oddsum = 0;
+// let divby5 = 0;
+// for (let i = 1; i <= num; i++) {
+//   console.log(i);
+
+//   if (i % 5 == 0) {
+//     console.log("Divisible by 5");
+//     divby5 = divby5 + 1;
+//   }
+
+//   if (i % 2 != 0) {
+//     oddsum = oddsum + i;
+//   }
+ 
+// }
+
+// console.log("count of the numbers which is divisible by 5 is:", divby5);
+// console.log("Sum of the odd numbers is", oddsum);
+
+
+
+
+// exercise 10
+let name = ["akhtar","fazal","zaid","umar","tihami"];
+let mark = [94,33,76,69,97];
+let rand = Math.random();
+let index;
+
+if(rand < 0.20){
+ index=0;
+}
+else if (rand >= 0.20 && rand < 0.40){
+ index=1;
+}
+else if(rand>=0.40 && rand <0.60){
+  index=2;
+}
+else if(rand >=0.60 && rand <0.80){
+  index=3;
 }
 else{
-  console.log("odd")
-  odd=odd+1;
-}
+  index=4;
 }
 
-let sum=0;
-for (let i = 1; i <= num; i++) {
-  sum = sum + i;
-}
-  console.log("sum of the above number is:",sum);
-  console.log("even number:",even);
-  console.log("odd number:",odd);
+let student = {
+    name:name[index],
+    mark:mark[index],
+    course:"BCA"
+  };
 
+  function grade (m){
+    if(m>=90){
+      return "Grade A";
+    }
+    else if(m>=75 && m<=89){
+      return "Grade B";
+    }
+    else if(m>=60 && m<=74){
+      return "Grade C";
+    }
+    else if(m>=40 && m<=59){
+      return "Grade D"
+    }
+    else{
+      return "FAIL";
+    }
+  }
 
-
+  let result = grade(student.mark)
+  console.log(student);
+  console.log(result);
