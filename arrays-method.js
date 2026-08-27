0e0a9dfde1f1feb0639f89d9702b83d2288cca3b
Akhtar Raza <akhtarraza1912@gmail.com>
2026-08-27 16:22:50 +0530

    practiced array methods

---
+++ arrays-method.js
@@ -0,0 +1,28 @@
+// let fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+// console.log(fruits[0])
+// console.log(fruits[3])
+
+// fruits[2]="Grapes"
+
+// console.log(fruits.length)
+// console.log(fruits)
+
+
+
+
+
+
+let items = ["Milk", "Bread", "Eggs"];
+
+items.push("Butter")
+console.log(items)
+
+items.unshift("Rice")
+console.log(items)
+
+items.pop()
+console.log(items)
+
+items.shift()
+console.log(items)
