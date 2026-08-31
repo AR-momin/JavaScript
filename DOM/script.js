3f0f9f22314f5c42e31d4ee9d58b08189fc5db9a
Akhtar Raza <akhtarraza1912@gmail.com>
2026-08-31 16:14:45 +0530

    practices different types of selector in DOM

---
+++ DOM/script.js
@@ -0,0 +1,33 @@
+// let boxes = document.querySelectorAll(".box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+
+
+
+
+// document.getElementById("title").innerHTML = "DOM practice started"
+// let ele = document.querySelectorAll(".info")
+// ele[1].style.color= "blue"
+// console.log(ele)
+
+
+
+
+
+// document.querySelector(".card").style.backgroundColor = "yellow"
+// let cards = document.querySelectorAll(".card")
+// cards[2].style.color = "red"
+// document.getElementById("message").innerHTML = "DOM Selection clear"
+// console.log(cards)
+
+
+
+
+document.getElementById("heading").innerHTML = "DOM Practice Complete"
+document.querySelector(".item").style.color = "green"
+let items = document.querySelectorAll(".item")
+items[1].style.backgroundColor = "pink"
+items[3].innerHTML = "Last item"
+console.log(items)
