0e33930249b2f0a7a2d71cff162d0a4112af2c0a
Akhtar Raza <akhtarraza1912@gmail.com>
2026-09-02 07:01:49 +0530

    Random Number, BackgroungColor, Temperature, and Ratings

--- DOM/color-box.js
+++ DOM/color-box.js
@@ -77,11 +77,98 @@
 
 
 
-let std = document.querySelectorAll(".student")
+// let std = document.querySelectorAll(".student")
 
-function getRandomMarks() {
-  let m = Math.floor(Math.random() * 100) + 1
-  return m;
+// function getRandomMarks() {
+//   let m = Math.floor(Math.random() * 100) + 1
+//   return m;
+// }
+
+// function getRandomBackgroundColor() {
+//   let v1 = Math.ceil(Math.random() * 255)
+//   let v2 = Math.ceil(Math.random() * 255)
+//   let v3 = Math.ceil(Math.random() * 255)
+//   return `rgb(${v1},${v2},${v3})`
+// }
+
+
+// function checkResult(marks){
+//   if(marks>=40){
+//     return "Pass"
+//   }
+//   else{
+//     return "Fail"
+//   }
+// }
+
+// std.forEach(e=>{
+//   let marks = getRandomMarks()
+//   let result = checkResult(marks)
+//   e.style.backgroundColor = getRandomBackgroundColor()
+// e.innerHTML = `Marks:${marks} - ${result}`
+
+//   if(result == "Pass"){
+//     e.style.color = "green"
+//   }
+//   else{
+//     e.style.color = "red"
+//   }
+
+// })
+
+
+
+
+
+
+// let products = document.querySelectorAll(".product")
+
+// function getRandomRating(){
+//   let rate = Math.floor(Math.random()*5)+1
+//   return rate;
+// }
+
+// function getRandomBackgroundColor(){
+//   let v1 = Math.ceil(Math.random()*255)
+//   let v2 = Math.ceil(Math.random()*255)
+//   let v3 = Math.ceil(Math.random()*255)
+//   return `rgb(${v1},${v2},${v3})`
+// }
+
+// function checkRating(r){
+//   if(r>=4){
+//     return "Recommended"
+//   }
+//   else{
+//     return "Normal"
+//   }
+// }
+
+// products.forEach(e=>{
+//   let rating = getRandomRating()
+//   let result = checkRating(rating)
+//   e.style.backgroundColor = getRandomBackgroundColor()
+//   e.innerHTML = `Rating: ${rating} - ${result}`
+
+//   if(result == "Recommended"){
+//     e.style.color = "green"
+//   }
+//   else{
+//     e.style.color = "red"
+//   }
+// })
+
+
+
+
+
+
+
+let cities = document.querySelectorAll(".city")
+
+function getRandomTemp() {
+  let temp = Math.floor(Math.random() * 36) + 10
+  return temp;
 }
 
 function getRandomBackgroundColor() {
@@ -91,17 +178,25 @@ function getRandomBackgroundColor() {
   return `rgb(${v1},${v2},${v3})`
 }
 
-
-function checkResult(mark){
-  if(mark>=40){
-    return "Pass"
+function checkTemp(t) {
+  if (t >= 30) {
+    return "Hot"
   }
-  else{
-    return "Fail"
+  else {
+    return "Normal"
   }
 }
 
-std.forEach(e=>{
+cities.forEach(e => {
+  let temp = getRandomTemp()
+  let result = checkTemp(temp)
   e.style.backgroundColor = getRandomBackgroundColor()
-  e.innerHTML = 
+  e.innerHTML = `Temperature: ${temp}°c - ${result} `
+
+  if (result == "Hot") {
+    e.style.color = "red"
+  }
+  else {
+    e.style.color = "blue"
+  }
 })
